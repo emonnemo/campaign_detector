51cbb96cb5b16d7ba7f8ce5322a05c5d3ecf6e84
Gisela <giselasupardi@gmail.com>
2018-11-12 00:16:33 +0700

    Add styling for scrapping page and menu button

--- campaign_detector/campaign_detector/views/scrap.py
+++ campaign_detector/campaign_detector/views/scrap.py
@@ -15,16 +15,17 @@ class ScrapView(APIView):
         return HttpResponse(html)
 
     def post(self, request, format=None):
+        results = ['Non-kampanye', 'Kubu 01', 'Kubu 02']
         response = requests.get('http://localhost:8000/api/v1/predict/')
         tweets = response.json().get('tweets')
         predictions = response.json().get('predictions1')
         predicted_data = []
-        for idx in range(len(tweets) - 1):
+        for idx in range(len(tweets)):
             predicted_data.append({
                 'tweet' : tweets[idx]['text'],
                 'username': tweets[idx]['username'],
                 'time': tweets[idx]['time'],
-                'prediction': predictions[idx][0],
+                'prediction': results[predictions[idx][0]],
             })
 
         template = get_template('scrap.html')
